db400e4ab1c09a02090bfba81eec8f5661fc5cc7
Murray Wang <rui@mimixiche.com>
2025-05-14 16:24:14 +0800

    fix(api): correct parameter name from column_id to channel_id

--- deepsearcher/rbase/ai_models.py
+++ deepsearcher/rbase/ai_models.py
@@ -435,7 +435,7 @@ def _create_params_by_summary_request(request: SummaryRequest) -> dict:
         }
     elif request.related_type == RelatedType.COLUMN:
         params = {
-            "column_id": request.related_id
+            "channel_id": request.related_id
         }
     elif request.related_type == RelatedType.ARTICLE:
         params = {
